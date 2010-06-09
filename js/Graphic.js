3a6936cdb86567f78a5d1a307c821e26182ed019
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2010-06-09 14:55:55 -0400

    Proto-Graphic

--- js/Graphic.js
+++ js/Graphic.js
@@ -1,3 +1,30 @@
 /*
 	Classe Graphic descreve as propriedades do Canvas.
 */
+
+function Graphic(canvas, divx, divy){
+	this.canvas = canvas;
+	this.ctx = this.canvas.ctx('2d');
+
+	this.div.x = divx;
+	this.div.y = divy;
+
+	this.dx = this.canvas.width / this.div.x;
+	this.dy = this.canvas.height / this.div.y;
+
+}
+
+Graphic.prototype.render(Worm){
+	this.ctx.save();
+	this.ctx.beginPath();
+	var x, y;
+
+	for(var celula in Worm.corpo){
+		x = celula.x * this.dx;
+		y = celula.y * this.dy;
+		this.ctx.rect(x, y, this.dx, this.dy);
+	}
+
+	this.ctx.fill();
+	this.ctx.restore();
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 /*
 	Descreve o Funcionamento do Jogo
 */
+var game;
+
+function gameInit(){
+	if(Graphic && Worm && Nibbles){
+		game = new Nibbles;
+		game.start();
+	}
+	else{
+		setTimeOut(gameInit, 150);
+	}
+
+}
+
+
+window.onload = gameInit;
