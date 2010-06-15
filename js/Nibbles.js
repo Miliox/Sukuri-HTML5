518b95d6ac1cf014317c696d411c7b74e6905d9c
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2010-06-15 19:46:52 -0400

    adicionado som

--- js/Nibbles.js
+++ js/Nibbles.js
@@ -19,6 +19,9 @@ function Nibbles(canvas, worms) {
 	this.level = 1;
 	this.maxScore = 0;
 	this.fps = this.DEFAULTFPS;
+	
+	//Sound Effect
+	this.eatSound = new Audio("audio/beep-21.ogg");	
 
 	//ID do Evento
 	this.loopCode = null;
@@ -27,20 +30,14 @@ function Nibbles(canvas, worms) {
 Nibbles.prototype.start = function () {
 		//Registra LoopGame
 		this.registerLoopGame();
-		//Registra Input Listener
-		if (!document.addEventListener && document.attachEvent){
-			document.attachEvent('onkeydown', this.keyboardInput);
-		} else {
-			window.addEventListener('keydown', this.keyboardInput, true);
-		}
-};
 
+};
 Nibbles.prototype.gameOver = function () {
 	//Restaura Velocidade Inicial
 	this.level = 0;
 	this.ate = 0;
 	this.fps = this.DEFAULTFPS;
-	window.clearInterval(loopCode);
+	window.clearInterval(this.loopCode);
 	this.registerLoopGame();
 	//Reinicia minhoca
 	this.worms[0].restart();
@@ -75,6 +72,7 @@ Nibbles.prototype.loopGame = function () {
 		//Cresce um pouco, se comeu
 		if (this.food.visible && worm.corpo[0].equals(this.food.pos)) {
 			worm.addScore(this.POINT);
+			this.eatSound.play();
 			this.food.visible = false;
 			this.food.randomTime(5);
 			this.food.randomPosition();
@@ -115,35 +113,7 @@ Nibbles.prototype.loopGame = function () {
 	this.display.render(this.worms, this.food, this.maxScore, this.level);
 };
 
-Nibbles.prototype.keyboardInput = function (event) {
-	var teclado = {up:38, down: 40, left: 37, right: 39, plus: 107, minus: 109};
 
-	switch (event.keyCode){
-		//Player controls
-		case teclado.up:
-			if (this.worms[0].direcao != 1) {
-				this.worms[0].direcaoPretendida = 0;
-			}
-			break;
-		case teclado.down:
-			if (this.worms[0].direcao != 0) {
-				this.worms[0].direcaoPretendida = 1;
-			}
-			break;
-		case teclado.left:
-			if (this.worms[0].direcao != 3) {
-				this.worms[0].direcaoPretendida = 2;
-			}
-			break;
-		case teclado.right:
-			if (this.worms[0].direcao != 2) {
-				this.worms[0].direcaoPretendida = 3;
-			}
-			break;
-		default:
-			break;
-	}
-};
 
 Nibbles.prototype.registerLoopGame = function () {
 	self = this;
--- js/main.js
+++ js/main.js
@@ -18,7 +18,45 @@ function gameInit(){
 					new Vector(2, 3),
 					new Vector(1, 3)],
 					3)]);
+		//Registra Controle
+		//Registra Input Listener
+		if (!document.addEventListener && document.attachEvent){
+			document.attachEvent('onkeydown', keyboardInput);
+		} else {
+			window.addEventListener('keydown', keyboardInput, true);
+		}
+		//Inicia Jogo
 		game.start();
 	}
 }
+
+function keyboardInput (event) {
+	var teclado = {up:38, down: 40, left: 37, right: 39, plus: 107, minus: 109};
+	
+	switch (event.keyCode){
+		//Player controls
+		case teclado.up:
+			if (game.worms[0].direcao != 1) {
+				game.worms[0].direcaoPretendida = 0;
+			}
+			break;
+		case teclado.down:
+			if (game.worms[0].direcao != 0) {
+				game.worms[0].direcaoPretendida = 1;
+			}
+			break;
+		case teclado.left:
+			if (game.worms[0].direcao != 3) {
+				game.worms[0].direcaoPretendida = 2;
+			}
+			break;
+		case teclado.right:
+			if (game.worms[0].direcao != 2) {
+				game.worms[0].direcaoPretendida = 3;
+			}
+			break;
+		default:
+			break;
+	}
+};
 window.onload = gameInit;
