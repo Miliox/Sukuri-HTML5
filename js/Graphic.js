ebc50a5d933847c09b48ba0cd66564ce9551f796
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2010-06-18 00:10:31 -0400

    removido trechos substituidos e modificado formatacao

--- js/Graphic.js
+++ js/Graphic.js
@@ -27,90 +27,35 @@ function Graphic(canvas, tileX, tileY){
 
 	this.BACKGROUNDS = ["white","green","yellow","DarkSlategray", "orange"];
 }
-
 Graphic.prototype.render = function (worms, food, MAX_SCORE, level) {
 	this.ctxBuffer.save();
 
-	//Limpa Buffer
 	this.renderBufferBackground(level - 1);
-	/*
-	this.ctxBuffer.save();
-	this.ctxBuffer.fillStyle = this.BACKGROUNDS[(level - 1)% this.BACKGROUNDS.length];
-	this.ctxBuffer.fillRect(0,0,this.canvasBuffer.width,this.canvasBuffer.height);
-	this.ctxBuffer.restore();
-	*/
-	var x, y;
-	this.ctxBuffer.beginPath();
-	//Renderiza Worms
+	//renderiza worms
 	for(var i = 0;i < worms.length; i++){
-		//renderiza um worm por vez
 		this.renderBufferWorm(worms[i]);
-		/*this.ctxBuffer.save();
-		this.ctxBuffer.beginPath();
-		for(var i = 0;i <  worms[j].corpo.length; i++){
-			x = worms[j].corpo[i].x * this.TILEWIDTH;
-			y = worms[j].corpo[i].y * this.TILEHEIGHT;
-			this.ctxBuffer.rect(x, y, this.TILEWIDTH, this.TILEHEIGHT);
-		}
-		this.ctxBuffer.fillStyle = worms[j].color;
-		this.ctxBuffer.fill();
-		this.ctxBuffer.restore();*/
 	}
-
-	//Renderiza Food
 	this.ctxBuffer.beginPath();
+
+	//renderiza food
 	if (food.isVisible()) {
 		this.renderBufferDiamond(food);
-		/*this.ctxBuffer.fillStyle = food.style;
-		x = food.getPos().x * this.TILEWIDTH;
-		y = food.getPos().y * this.TILEHEIGHT;
-		this.ctxBuffer.rect(x, y, this.TILEWIDTH, this.TILEHEIGHT);
-		this.ctxBuffer.fill();
-		if(food.isToxic()){
-			this.ctxBuffer.stroke();
-		}*/
 	}
-	//Renderiza Score
+	//renderiza scores
 	for(var i = 0;i < worms.length; i++){
 		this.renderBufferScore(i+1, worms[i].score, (i * 125)+ 10);
 	}
-	/*this.ctxBuffer.fillStyle = "black";
-	this.ctxBuffer.strokeStyle = "white";
-	this.ctxBuffer.font = "12pt Arial";
-	var content = "Score: " + worms[0].score + "pt ";
-	this.ctxBuffer.fillText(content, 5, this.canvasBuffer.height - 10);
-	*/
 	this.renderBufferLevel(level);
-	/*
-	var content = "Nivel: " + level;
-	this.ctxBuffer.fillText(content, 5, 5);
-	*/
 	this.renderBufferRecord(MAX_SCORE);
-	/*
-	content = "Recorde: " + MAX_SCORE + "pt ";
-	this.ctxBuffer.fillText(content, 5, 25);
-	*/
-	//Renderiza Propaganda
+
+	//renderiza propaganda
 	this.renderBufferTitle("SUKURI");
-	/*this.ctxBuffer.textAlign = 'center';
-	this.ctxBuffer.textBaseline = 'middle';
-	this.ctxBuffer.fillText("SUKURI",(this.canvasBuffer.width / 2), 12);
-	*/
 	this.renderBufferCopyright();
-	/*this.ctxBuffer.textAlign = 'end';
-	this.ctxBuffer.font = "8pt Arial";
-	this.ctxBuffer.fillText("\u00A9POWERED BY LABORATÓRIO DE PÓS GRADUAÇÃO",
-			this.canvasBuffer.width - 10, this.canvasBuffer.height - 10);
-	*/
 	this.renderBufferVersion("0.02");
-	/*this.ctxBuffer.fillText("versão: 0.02",this.canvasBuffer.width - 10, 10);
-	this.ctxBuffer.restore();
-	*/
-	//Aplica Buffer Secundario no Principal
+
 	this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
 	this.ctx.drawImage(this.canvasBuffer,0,0);
 };
-
 Graphic.prototype.renderBufferBackground = function (value) {
 	this.ctxBuffer.save();
 	this.ctxBuffer.fillStyle = this.BACKGROUNDS[ value % this.BACKGROUNDS.length];
@@ -133,6 +78,7 @@ Graphic.prototype.renderBufferWorm = function (worm) {
 };
 Graphic.prototype.renderBufferDiamond = function (food){
 	this.ctxBuffer.save();
+	this.ctxBuffer.beginPath();
 	this.ctxBuffer.fillStyle = food.style;
 	var x = food.getPos().x * this.TILEWIDTH;
 	var y = food.getPos().y * this.TILEHEIGHT;
@@ -147,54 +93,45 @@ Graphic.prototype.renderBufferScore = function (i, score, position) {
 	var text_score = "Worm_" + i + ": " + score + "pt ";
 
 	this.ctxBuffer.save();
-
-	this.ctxBuffer.textAlign = "start";
-	this.ctxBuffer.textBaseline = "alphabetic"
+	this.setBufferTextFormat('start','alphabetic','8pt Verdana');
 	this.ctxBuffer.fillStyle = "black";
-	this.ctxBuffer.strokeStyle = "white";
-	this.ctxBuffer.font = "12pt Arial";
 	this.ctxBuffer.fillText(text_score, position, this.canvasBuffer.height - 10);
 
 	this.ctxBuffer.restore();
 };
 Graphic.prototype.renderBufferTitle = function (text) {
 	this.ctxBuffer.save();
-	this.ctxBuffer.textAlign = 'center';
-	this.ctxBuffer.textBaseline = 'top';
-	this.ctxBuffer.font = '12pt Arial';
+	this.setBufferTextFormat('center','top','12pt Arial');
 	this.ctxBuffer.fillText(text,(this.canvasBuffer.width / 2), 12);
 	this.ctxBuffer.restore();
 };
 Graphic.prototype.renderBufferCopyright = function (){
 	var copyright = "\u00A9POWERED BY LABORATÓRIO DE PÓS GRADUAÇÃO";
 	this.ctxBuffer.save();
-	this.ctxBuffer.textAlign = 'end';
-	this.ctxBuffer.textBaseline = 'bottom';
-	this.ctxBuffer.font = "8pt Arial";
-	this.ctxBuffer.fillText(copyright,this.canvasBuffer.width - 10, this.canvasBuffer.height - 10);
+	this.setBufferTextFormat('end','bottom','8pt Arial');
+	this.ctxBuffer.fillText(copyright,this.canvasBuffer.width, this.canvasBuffer.height);
 	this.ctxBuffer.restore();
 };
 Graphic.prototype.renderBufferVersion = function (version) {
 	this.ctxBuffer.save();
-	this.ctxBuffer.textAlign = 'end';
-	this.ctxBuffer.textBaseline = 'middle';
-	this.ctxBuffer.font = "8pt Arial";
+	this.setBufferTextFormat('end','middle','8pt Arial');
 	this.ctxBuffer.fillText("v." + version,this.canvasBuffer.width - 10, 10);
 	this.ctxBuffer.restore();
 };
 Graphic.prototype.renderBufferLevel = function (level){
 	this.ctxBuffer.save();
-	this.ctxBuffer.textAlign = 'start';
-	this.ctxBuffer.textBaseline = 'top';
-	this.ctxBuffer.font = "8pt Arial";
+	this.setBufferTextFormat('start','top','8pt Verdana');
 	this.ctxBuffer.fillText("Nivel: " + level, 5, 5);
 	this.ctxBuffer.restore();
 };
 Graphic.prototype.renderBufferRecord = function (recorde){
 	this.ctxBuffer.save();
-	this.ctxBuffer.textAlign = 'start';
-	this.ctxBuffer.textBaseline = 'top';
-	this.ctxBuffer.font = "8pt Arial";
-	this.ctxBuffer.fillText("Recored: " + recorde +"pt", 5, 25);
+	this.setBufferTextFormat('start','top','8pt Verdana');
+	this.ctxBuffer.fillText("Record: " + recorde +"pt", 80, 5);
 	this.ctxBuffer.restore();
+};
+Graphic.prototype.setBufferTextFormat = function (align, baseline, font){
+	this.ctxBuffer.textAlign = align;
+	this.ctxBuffer.textBaseline = baseline;
+	this.ctxBuffer.font = font;
 };
