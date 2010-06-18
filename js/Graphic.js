16d0c7cf96209c7418449ffa07e9d0e5f1a3d8af
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2010-06-17 23:43:56 -0400

    Classe Graphics reorganizada para melhorar legibilidade

--- js/Graphic.js
+++ js/Graphic.js
@@ -32,16 +32,20 @@ Graphic.prototype.render = function (worms, food, MAX_SCORE, level) {
 	this.ctxBuffer.save();
 
 	//Limpa Buffer
+	this.renderBufferBackground(level - 1);
+	/*
 	this.ctxBuffer.save();
 	this.ctxBuffer.fillStyle = this.BACKGROUNDS[(level - 1)% this.BACKGROUNDS.length];
 	this.ctxBuffer.fillRect(0,0,this.canvasBuffer.width,this.canvasBuffer.height);
 	this.ctxBuffer.restore();
+	*/
 	var x, y;
 	this.ctxBuffer.beginPath();
 	//Renderiza Worms
-	for(var j = 0;j < worms.length; j++){
+	for(var i = 0;i < worms.length; i++){
 		//renderiza um worm por vez
-		this.ctxBuffer.save();
+		this.renderBufferWorm(worms[i]);
+		/*this.ctxBuffer.save();
 		this.ctxBuffer.beginPath();
 		for(var i = 0;i <  worms[j].corpo.length; i++){
 			x = worms[j].corpo[i].x * this.TILEWIDTH;
@@ -50,44 +54,147 @@ Graphic.prototype.render = function (worms, food, MAX_SCORE, level) {
 		}
 		this.ctxBuffer.fillStyle = worms[j].color;
 		this.ctxBuffer.fill();
-		this.ctxBuffer.restore();
+		this.ctxBuffer.restore();*/
 	}
 
 	//Renderiza Food
 	this.ctxBuffer.beginPath();
-	if (food.visible) {
-		this.ctxBuffer.fillStyle = food.style;
+	if (food.isVisible()) {
+		this.renderBufferDiamond(food);
+		/*this.ctxBuffer.fillStyle = food.style;
 		x = food.getPos().x * this.TILEWIDTH;
 		y = food.getPos().y * this.TILEHEIGHT;
 		this.ctxBuffer.rect(x, y, this.TILEWIDTH, this.TILEHEIGHT);
 		this.ctxBuffer.fill();
 		if(food.isToxic()){
 			this.ctxBuffer.stroke();
-		}
+		}*/
 	}
 	//Renderiza Score
-	this.ctxBuffer.fillStyle = "black";
+	for(var i = 0;i < worms.length; i++){
+		this.renderBufferScore(i+1, worms[i].score, (i * 125)+ 10);
+	}
+	/*this.ctxBuffer.fillStyle = "black";
 	this.ctxBuffer.strokeStyle = "white";
 	this.ctxBuffer.font = "12pt Arial";
 	var content = "Score: " + worms[0].score + "pt ";
 	this.ctxBuffer.fillText(content, 5, this.canvasBuffer.height - 10);
-	content = "Nivel: " + level;
-	this.ctxBuffer.fillText(content, 5, this.canvasBuffer.height - 25);
+	*/
+	this.renderBufferLevel(level);
+	/*
+	var content = "Nivel: " + level;
+	this.ctxBuffer.fillText(content, 5, 5);
+	*/
+	this.renderBufferRecord(MAX_SCORE);
+	/*
 	content = "Recorde: " + MAX_SCORE + "pt ";
 	this.ctxBuffer.fillText(content, 5, 25);
-
+	*/
 	//Renderiza Propaganda
-	this.ctxBuffer.textAlign = 'center';
+	this.renderBufferTitle("SUKURI");
+	/*this.ctxBuffer.textAlign = 'center';
 	this.ctxBuffer.textBaseline = 'middle';
 	this.ctxBuffer.fillText("SUKURI",(this.canvasBuffer.width / 2), 12);
-	this.ctxBuffer.textAlign = 'end';
+	*/
+	this.renderBufferCopyright();
+	/*this.ctxBuffer.textAlign = 'end';
 	this.ctxBuffer.font = "8pt Arial";
 	this.ctxBuffer.fillText("\u00A9POWERED BY LABORATÓRIO DE PÓS GRADUAÇÃO",
 			this.canvasBuffer.width - 10, this.canvasBuffer.height - 10);
-	this.ctxBuffer.fillText("versão: 0.02",this.canvasBuffer.width - 10, 10);
+	*/
+	this.renderBufferVersion("0.02");
+	/*this.ctxBuffer.fillText("versão: 0.02",this.canvasBuffer.width - 10, 10);
 	this.ctxBuffer.restore();
+	*/
 	//Aplica Buffer Secundario no Principal
 	this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);
 	this.ctx.drawImage(this.canvasBuffer,0,0);
 };
 
+Graphic.prototype.renderBufferBackground = function (value) {
+	this.ctxBuffer.save();
+	this.ctxBuffer.fillStyle = this.BACKGROUNDS[ value % this.BACKGROUNDS.length];
+	this.ctxBuffer.fillRect(0,0,this.canvasBuffer.width,this.canvasBuffer.height);
+	this.ctxBuffer.restore();
+};
+Graphic.prototype.renderBufferWorm = function (worm) {
+	var x, y;
+
+	this.ctxBuffer.save();
+	this.ctxBuffer.beginPath();
+	for(var i = 0;i <  worm.corpo.length; i++){
+		x = worm.corpo[i].x * this.TILEWIDTH;
+		y = worm.corpo[i].y * this.TILEHEIGHT;
+		this.ctxBuffer.rect(x, y, this.TILEWIDTH, this.TILEHEIGHT);
+	}
+	this.ctxBuffer.fillStyle = worm.color;
+	this.ctxBuffer.fill();
+	this.ctxBuffer.restore();
+};
+Graphic.prototype.renderBufferDiamond = function (food){
+	this.ctxBuffer.save();
+	this.ctxBuffer.fillStyle = food.style;
+	var x = food.getPos().x * this.TILEWIDTH;
+	var y = food.getPos().y * this.TILEHEIGHT;
+	this.ctxBuffer.rect(x, y, this.TILEWIDTH, this.TILEHEIGHT);
+	this.ctxBuffer.fill();
+	if(food.isToxic()){
+		this.ctxBuffer.stroke();
+	}
+	this.ctxBuffer.restore();
+};
+Graphic.prototype.renderBufferScore = function (i, score, position) {
+	var text_score = "Worm_" + i + ": " + score + "pt ";
+
+	this.ctxBuffer.save();
+
+	this.ctxBuffer.textAlign = "start";
+	this.ctxBuffer.textBaseline = "alphabetic"
+	this.ctxBuffer.fillStyle = "black";
+	this.ctxBuffer.strokeStyle = "white";
+	this.ctxBuffer.font = "12pt Arial";
+	this.ctxBuffer.fillText(text_score, position, this.canvasBuffer.height - 10);
+
+	this.ctxBuffer.restore();
+};
+Graphic.prototype.renderBufferTitle = function (text) {
+	this.ctxBuffer.save();
+	this.ctxBuffer.textAlign = 'center';
+	this.ctxBuffer.textBaseline = 'top';
+	this.ctxBuffer.font = '12pt Arial';
+	this.ctxBuffer.fillText(text,(this.canvasBuffer.width / 2), 12);
+	this.ctxBuffer.restore();
+};
+Graphic.prototype.renderBufferCopyright = function (){
+	var copyright = "\u00A9POWERED BY LABORATÓRIO DE PÓS GRADUAÇÃO";
+	this.ctxBuffer.save();
+	this.ctxBuffer.textAlign = 'end';
+	this.ctxBuffer.textBaseline = 'bottom';
+	this.ctxBuffer.font = "8pt Arial";
+	this.ctxBuffer.fillText(copyright,this.canvasBuffer.width - 10, this.canvasBuffer.height - 10);
+	this.ctxBuffer.restore();
+};
+Graphic.prototype.renderBufferVersion = function (version) {
+	this.ctxBuffer.save();
+	this.ctxBuffer.textAlign = 'end';
+	this.ctxBuffer.textBaseline = 'middle';
+	this.ctxBuffer.font = "8pt Arial";
+	this.ctxBuffer.fillText("v." + version,this.canvasBuffer.width - 10, 10);
+	this.ctxBuffer.restore();
+};
+Graphic.prototype.renderBufferLevel = function (level){
+	this.ctxBuffer.save();
+	this.ctxBuffer.textAlign = 'start';
+	this.ctxBuffer.textBaseline = 'top';
+	this.ctxBuffer.font = "8pt Arial";
+	this.ctxBuffer.fillText("Nivel: " + level, 5, 5);
+	this.ctxBuffer.restore();
+};
+Graphic.prototype.renderBufferRecord = function (recorde){
+	this.ctxBuffer.save();
+	this.ctxBuffer.textAlign = 'start';
+	this.ctxBuffer.textBaseline = 'top';
+	this.ctxBuffer.font = "8pt Arial";
+	this.ctxBuffer.fillText("Recored: " + recorde +"pt", 5, 25);
+	this.ctxBuffer.restore();
+};
