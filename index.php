3e66479cfd1be5178557392942a984d86ef4d8e8
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2010-06-21 23:06:55 -0400

    separado estilo css do index.php

--- index.php
+++ index.php
@@ -16,19 +16,7 @@
 		<script type="text/javascript" src="js/intro.js"></script>
 		<script type="text/javascript" src="js/main.js"></script>
 		<style type="text/css">
-			canvas {
-				/*Centraliza*/
-				position: absolute;
-				top: 50%;
-				left: 50%;
-				
-				/*Corrigi alinhamento para o centro*/
-				margin-left: -480px;
-				margin-top: -250px;
-
-				/*Destaca o Elemento do Fundo*/
-				border: 1px solid black;
-			}
+			@import url("css/styles.css");	
 		</style>
 	</head>
 	<body>
