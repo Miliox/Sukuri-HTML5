5236166e9e132f443191aaad2224722b1cd4cb4c
Emiliano Carlos de Moraes Firmino <elmiliox@gmail.com>
2010-06-24 23:45:47 -0400

    removido de gameInit verificacao de existencia de classes por nao ter funcionado

--- js/main.js
+++ js/main.js
@@ -2,32 +2,20 @@
 var game;
 
 function gameInit(){
-	/*Execute o Jogo*/
-	if (Graphic === undefined ||
-		Nibbles === undefined ||
-		Vector === undefined||
-		Worm === undefined ||
-		Diamond === undefined ||
-		Matriz === undefined) {
-		//Aguarda ate que as Classes estejam carregadas
-		window.setTimeOut(gameInit, 150);
-	}
-	else {
 		//Inicializa Jogo
-		var w0body = [new Vector(4,3),new Vector(3,3),new Vector(2,3),new Vector(1,3)];
-		var w1body = [new Vector(91,43),new Vector(92,43),new Vector(93,43),new Vector(94,43)];
-		var w2body = [new Vector(4,43),new Vector(3,43),new Vector(2,43),new Vector(1,43)];
-		var w3body = [new Vector(91,3),new Vector(92,3),new Vector(93,3),new Vector(94,3)];
-		var w0 = new WormHuman(w0body,"left", "red");
-		var w1 = new WormBot(w1body,"right", "blue");
-		var w2 = new WormBot(w2body,"left", "yellow");
-		var w3 = new WormBot(w3body,"right", "green");
-		game = new Nibbles(document.getElementById('nibbles'),[w0,w1,w2,w3]);
-		//Registra Controle
-		window.addEventListener('keydown', keyboardInput, true);
-		//Inicia Jogo
-		game.start();
-	}
+	var w0body = [new Vector(4,3),new Vector(3,3),new Vector(2,3),new Vector(1,3)];
+	var w1body = [new Vector(91,43),new Vector(92,43),new Vector(93,43),new Vector(94,43)];
+	var w2body = [new Vector(4,43),new Vector(3,43),new Vector(2,43),new Vector(1,43)];
+	var w3body = [new Vector(91,3),new Vector(92,3),new Vector(93,3),new Vector(94,3)];
+	var w0 = new WormHuman(w0body,"left", "red");
+	var w1 = new WormBot(w1body,"right", "blue");
+	var w2 = new WormBot(w2body,"left", "yellow");
+	var w3 = new WormBot(w3body,"right", "green");
+	game = new Nibbles(document.getElementById('nibbles'),[w0,w1,w2,w3]);
+	//Registra Controle
+	window.addEventListener('keydown', keyboardInput, true);
+	//Inicia Jogo
+	game.start();
 }
 
 function keyboardInput (event) {
